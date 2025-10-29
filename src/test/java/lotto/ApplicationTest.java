7fd1b730823676c2ceec97143c3822926d35eb9b
SoungJuyeon <tjdwndus1325@gmail.com>
2025-10-29 22:39:23 +0900

    test: 구입금액이 long 보다 크면 예외 발생 테스트 코드

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.view.InputView;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -52,6 +55,14 @@ class ApplicationTest extends NsTest {
             runException("1000j");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
+    }
+
+    @DisplayName("구입금액이_long보다_크면_예외가_발생한다.")
+    @Test
+    void 구입금액이_long보다_크면_예외가_발생한다() {
+        String overLongValue = "9223372036854775808";
+        assertThatThrownBy(() -> Long.parseLong(overLongValue))
+                .isInstanceOf(NumberFormatException.class);
     }
 
     @Override
